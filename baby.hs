b4236a8a83c10b2ab97d46edf7ca06dee5f9f60f
Ben Frondigoun <ben.frondigoun@gmail.com>
2021-03-09 17:53:42 +0000

    Few more functions, learning about types and list comprehension.

--- baby.hs
+++ baby.hs
@@ -1,1 +1,15 @@
 doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                    then x
+                    else doubleMe x
+
+
+boomBang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum[1 | _ <- xs]
+
+factorial :: Integer -> Integer 
+factorial x = product [1..x]
