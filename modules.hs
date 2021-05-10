9a2d32edc6d381d2ea4bb1f75f802769623d68db
Ben Frondigoun <ben.frondigoun@gmail.com>
2021-05-10 18:55:36 +0100

    Introduction to basic Data.List and Data.Char modules, and some of the functions therein. Also some practice with folds and recursive functions, as shown in search and lookUp functions written

---
+++ modules.hs
@@ -0,0 +1,33 @@
+import qualified Data.List as M -- (nub, sort) would load only those two functions from the module
+import qualified Data.Char as C
+
+-- no. of unique items in a list
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . M.nub
+
+-- search for a sublist, e.g. search "ben" "my name is ben" = True
+search :: (Eq a) => [a] -> [a] -> Bool  
+search needle haystack =   
+    let nlen = length needle  
+    in  foldl (\acc x -> if M.take nlen x == needle then True else acc) False (M.tails haystack)  
+-- Data.List.isInfixOf function does the same job
+
+-- find function is very useful, safer than dropWhile if we may not get a return value. Learn about Maybe types
+
+
+-- Caesar cypher
+caesar :: Int -> String -> String 
+caesar key message = 
+    let nums = map C.ord message
+        shifted = map (+key) nums
+    in map C.chr shifted
+
+decodeCaesar :: Int -> String -> String 
+decodeCaesar key cyphertext = caesar (negate key) cyphertext
+
+-- simple dictionary lookup using Maybe types and other stuff
+lookUp :: (Eq k) => k -> [(k,v)] -> Maybe v
+lookUp key [] = Nothing 
+lookup key ((k,v):xs) = if key == k
+                        then Just v
+                        else lookUp key xs
